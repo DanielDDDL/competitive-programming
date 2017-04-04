0cda1b768b2abe0aaa77f13d693ece5cf3bc207b
Daniel Dias de Lima <daniel.dddl.pdf@gmail.com>
2017-04-04 16:25:40 -0300

    Fila - queue
    Estudando para a prova de estrutura de dados

---
+++ data_structures/fila_circular.py
@@ -0,0 +1,85 @@
+# uma fila cinco quatro operações básicas
+# enqueue, dequeue, first, isEmpty, isFull,
+# enqueue - acrescenta um item na parte da frente da fila
+# dequeue - remove um item da parte da trás da fila
+# first - retorna o primeiro da fila
+# isEmpty - retorna true se a fila se esta vazia; caso contrário, False
+# isFull - retorna True se a fila esta cheia; caso contrário, False
+
+import random # for tests purposes
+
+class Fila:
+    def __init__(self, qnt_items):
+        self.qnt_items = qnt_items
+        self.items = [None] * qnt_items
+        self.first_line = 0
+        self.last_line  = -1
+        self.size = 0
+
+    def is_empty (self):
+        return self.size == 0
+
+    def is_full (self):
+        return self.size == self.qnt_items
+
+    def first (self):
+        if self.is_empty():
+            raise RuntimeError ("Attempt to get the first item of an empty line")
+        else:
+            return self.items[self.first_line]
+
+    def enqueue (self, item):
+        if self.is_full():
+            raise RuntimeError ("Attempt to enqueue an item in a full queue")
+        else:
+            # because the queue is circular
+            # index of the last item comes back to the start if there is room for it
+            if self.last_line + 1 == self.qnt_items:
+                self.last_line = 0
+            else:
+                self.last_line += 1
+
+            self.items[self.last_line] = item
+            self.size += 1
+
+    def dequeue (self):
+        if self.is_empty():
+            raise RuntimeError ("Attempt to dequeue an item from a full queue")
+        else:
+            # because the queue is circular
+            # index of the first item comes back to the start if there is room for it
+            self.items[self.first_line] = None
+            self.size -= 1
+            if self.first_line == self.qnt_items - 1:
+                self.first_line = 0
+            else:
+                self.first_line += 1
+
+
+
+    def __str__ (self):
+        info  = "Items of the queue: " + str(self.items)
+        info += "\nIndex of the first item: " + str(self.first_line)
+        info += "\nIndex of the last item:" + str(self.last_line)
+        info += "\nSize of the queue: " + str(self.size)
+        info += "\nMax quantity of items allowed: " + str(self.qnt_items)
+        return info
+
+# ---------- TESTES ------------ #
+def main ():
+    TAMANHO = 10
+    f = Fila(TAMANHO)
+    print ("------IMPRIMINDO REMOCAO-------")
+    for i in range (TAMANHO):
+        f.enqueue(random.randint(1,10))
+        print (f.first())
+
+    print ("------IMPRIMINDO REMOCAO-------")
+    for i in range (TAMANHO):
+        print (f.first())
+        f.dequeue()
+
+
+
+if __name__ == "__main__":
+    main()
